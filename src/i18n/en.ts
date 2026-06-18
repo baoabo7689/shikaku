fc0bb4ac0755dc66da2b8a8bd319157afd242a86
Nguyen Trang Hong Bao <nthb@globeteam.com>
2026-06-18 11:48:52 +0700

    i

---
+++ src/i18n/en.ts
@@ -0,0 +1,20 @@
+export const translations = {
+  home: {
+    title: 'Welcome',
+    description: 'Welcome to the Shikaku Game. Start building your app here.',
+  },
+  header: {
+    title: 'Shikaku',
+    language: 'Language',
+    toggleLanguage: 'Change language',
+    contact: 'Contact',
+    help: 'Help',
+  },
+  help: {
+    title: 'Help',
+    common: 'Common',
+    commonHelps: ['Sample help for common section.'],
+  },
+};
+
+export default { translations };
